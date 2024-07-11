259559a99b8c9171f028d3f7839053abebb5a581
sebelumcahahaya <anandadimmas1204@gmail.com>
2024-07-11 12:32:51 +0700

    dim

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Ananda Dimmas Budiarto - Fullstack Developer And Devops</title>
         <meta name="description" content="Ananda Dimmas Budiarto, a Fullstack Developer with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
-        <meta name="keywords" content="Fullstack Developer, Ananda Dimmas Budiarto, Frontend Developer, Backend Developer, Web Development, JavaScript, PHP, Laravel, React, Next.js, Node.js, DevOps, CI/CD, Docker, Programmer Blora, Blora, Ketipung, Dimmas, Ananda Dimmas, Ananda Blora, Ananda Dimmas Blora, SMAN 1 JEPON" />
+        <meta name="keywords" content="Fullstack Developer, Ananda Dimmas Budiarto Fullstack Developer,Fullstack Developer Ananda Dimmas Budiarto, Frontend Developer, Backend Developer, Web Development, JavaScript, PHP, Laravel, React, Next.js, Node.js, DevOps, CI/CD, Docker, Programmer Blora, Blora, Ketipung, Dimmas, Ananda Dimmas, Ananda Blora, Ananda Dimmas Blora, SMAN 1 JEPON" />
         <meta property="og:title" content="Ananda Dimmas Budiarto - Fullstack Developer" />
         <meta property="og:description" content="Ananda Dimmas Budiarto, a Fullstack Developer with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
         <meta property="og:image" content="https://anandadimmas.online/packages/images/aku.jpeg" />
