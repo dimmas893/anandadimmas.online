196b270dba762e5f4026ce5148e4d77b907ef37f
sebelumcahahaya <anandadimmas1204@gmail.com>
2024-07-11 15:19:52 +0700

    123

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Ananda Dimmas Budiarto - Fullstack Developer And Devops</title>
         <meta name="google-site-verification" content="36eu7KctsJZNMVI-kV0XTCr_IylaXs4E60VsHjWek1U" />
-        <meta name="description" content="Ananda Dimmas Budiarto, a Fullstack Developer with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
+        <meta name="description" content="Ananda Dimmas Budiarto, a Fullstack Developer And Devops with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
         <meta name="keywords" content="Fullstack Developer, Ananda Dimmas Budiarto Fullstack Developer,Fullstack Developer Ananda Dimmas Budiarto, Frontend Developer, Backend Developer, Web Development, JavaScript, PHP, Laravel, React, Next.js, Node.js, DevOps, CI/CD, Docker, Programmer Blora, Blora, Ketipung, Dimmas, Ananda Dimmas, Ananda Blora, Ananda Dimmas Blora, SMAN 1 JEPON" />
-        <meta property="og:title" content="Ananda Dimmas Budiarto - Fullstack Developer" />
-        <meta property="og:description" content="Ananda Dimmas Budiarto, a Fullstack Developer with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
+        <meta property="og:title" content="Ananda Dimmas Budiarto - Fullstack Developer And Devops" />
+        <meta property="og:description" content="Ananda Dimmas Budiarto, a Fullstack Developer And Devops with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
         <meta property="og:image" content="https://anandadimmas.online/packages/images/aku.jpeg" />
         <meta property="og:url" content="https://anandadimmas.online" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Ananda Dimmas Budiarto - Fullstack Developer" />
-        <meta name="twitter:description" content="Ananda Dimmas Budiarto, a Fullstack Developer with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
+        <meta name="twitter:title" content="Ananda Dimmas Budiarto - Fullstack Developer And Devops" />
+        <meta name="twitter:description" content="Ananda Dimmas Budiarto, a Fullstack Developer And Devops with high-level experience in both front-end and back-end web development, creating robust and scalable applications with quality work." />
         <meta name="twitter:image" content="https://anandadimmas.online/packages/images/aku.jpeg" />
         <link rel="icon" href="https://anandadimmas.online/packages/images/icon.png" />
         {/*==================== UNICONS ====================*/}
